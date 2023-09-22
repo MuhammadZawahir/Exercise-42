7e14ed009db28e230051f4375c828019b05feb5a
Zawahir <139978657+MuhammadZawahir@users.noreply.github.com>
2023-09-22 17:33:02 +0500

    Exercise 42

---
+++ index.ts
@@ -0,0 +1,12 @@
+//Great Magicians: Start with a copy of your program from Exercise 40. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+let magicians:string[]=['Harry Houdini', 'David Blane', 'Shin Lim'];
+function make_great():any{
+    for (let magician of magicians){
+        magician='The Great ' + magician
+        console.log(magician);
+        
+    }
+}
+console.log("\n Great Magicians:\n");
+
+make_great();
